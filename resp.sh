064d3bb7e9c1b0115b8148a4fb3823e0bdd17456
Julietanilem <92052334+Julietanilem@users.noreply.github.com>
2023-06-14 15:34:43 -0600

    Update resp.sh

--- resp.sh
+++ resp.sh
@@ -1,11 +1,11 @@
-#!/bin/bash 
+#!/bin/bash
 #Insertar las respuestas en los archivos de texto de cada usuario
 usuarios=('AlejandraDMV' 'delfinagomeza' 'RicardoMeb' 'leninperezr' 'aguadiana' 'manolojim')
-
-for usuario in "${usuarios[@]}"; do
-    while IFS= read -r id_tweet; do
-        snscrape --jsonl twitter-search "conversation_id:$id_tweet lang:es" >> "${usuario}_tweetsResp.txt"
-    done < "${usuario}IDS.txt"
+dias=("4-3" "6-4" "6-5")
+for dia in "${dias[@]}"; do
+    for usuario in "${usuarios[@]}"; do
+        while IFS= read -r id_tweet; do
+            snscrape --jsonl twitter-search "conversation_id:$id_tweet lang:es" >> "${usuario}${dia}_tweetsResp.txt"
+        done < "${usuario}${dia}IDS.txt"
+    done
 done
-
-
