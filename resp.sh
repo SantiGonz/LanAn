1bf10f4f8bf137b07c45bb6eed6acd14fb0f75a6
Santiago González <93099527+SantiGonz@users.noreply.github.com>
2023-06-14 16:31:28 -0600

    respuesta con puro contenido en resp.sh

--- resp.sh
+++ resp.sh
@@ -2,10 +2,14 @@
 #Insertar las respuestas en los archivos de texto de cada usuario
 usuarios=('AlejandraDMV' 'delfinagomeza' 'RicardoMeb' 'leninperezr' 'aguadiana' 'manolojim')
 dias=("4-3" "6-4" "6-5")
+
 for dia in "${dias[@]}"; do
     for usuario in "${usuarios[@]}"; do
         while IFS= read -r id_tweet; do
-            snscrape --jsonl twitter-search "conversation_id:$id_tweet lang:es" >> "${usuario}${dia}_tweetsResp.txt"
+            content=$(snscrape --jsonl twitter-search "conversation_id:$id_tweet lang:es" | jq -r '.content')
+            if [[ ! -z "$content" ]]; then
+                echo "$content" >> "${usuario}${dia}_tweetsResp.txt"
+            fi
         done < "${usuario}${dia}IDS.txt"
     done
 done
