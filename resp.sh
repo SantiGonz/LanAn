4a11054eb876ec4bf14a96b3dbf9351b0a5e90d5
Julieta Flores <julietanilem@gmail.com>
2023-06-14 10:43:40 -0600

    script de bash para las respuestas

---
+++ resp.sh
@@ -0,0 +1,10 @@
+#Insertar las respuestas en los archivos de texto de cada usuario
+usuarios=('AlejandraDMV' 'delfinagomeza' 'RicardoMeb' 'leninperezr' 'aguadiana' 'manolojim')
+
+for usuario in "${usuarios[@]}"; do
+    while IFS= read -r id_tweet; do
+        snscrape --jsonl twitter-search "conversation_id:$id_tweet lang:es" >> "${usuario}_tweetsResp.txt"
+    done < "${usuario}IDS.txt"
+done
+
+
--- scrap.py
+++ scrap.py
@@ -2,13 +2,13 @@ from snscrape.modules import twitter
 import json
 from datetime import datetime
 import pytz
-
+#Sacar los IDS de todas las respuestas de un usuario en un rango de fechas
 usuarios = ['AlejandraDMV', 'delfinagomeza', 'RicardoMeb', 'leninperezr', 'aguadiana','manolojim']
 start_date = datetime(2023, 5, 25).replace(tzinfo=pytz.UTC)
 end_date = datetime(2023, 6, 4, 23, 59, 59).replace(tzinfo=pytz.UTC)
 
 for usuario in usuarios:
-    output_file = usuario.replace(" ", "_") + "_id's.txt"
+    output_file = usuario+"IDS.txt"
     with open(output_file, 'w') as f:
         print(usuario)
         scraper = twitter.TwitterUserScraper(usurio)
@@ -17,12 +17,12 @@ for usuario in usuarios:
             tweet_date = tweet.date
             if start_date <= tweet_date < end_date:
                 tweet_json = json.loads(tweet.json())
-                print(f"\nScraped tweet: {tweet_json['id']}")
+                print(f"\n{tweet_json['id']}")
                 f.write(tweet_json['id'])
                 f.write('\n')
                 f.flush()
                 i+=1
             elif tweet_date < end_date :
-                print("Fueron {} twits".format(i))
+                # print(f"Fueron {i} twits")
                 break
 
